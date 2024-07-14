4679e78942b8f5af7b3cbd2323f60e0713778c27
Alex <akp42@duke.edu>
2024-07-14 13:34:26 -0700

    init hs comp

---
+++ src/components/hubspot.js
@@ -0,0 +1,9 @@
+export default function Hubspot() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>This is a Hubspot component.</p>
+      </header>
+    </div>
+  );
+}
