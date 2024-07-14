37282ba7645ce369f54a1fa89e732109b80ad603
Alex <akp42@duke.edu>
2024-07-14 16:17:22 -0700

    viz

--- src/components/hubspot.js
+++ src/components/hubspot.js
@@ -2,7 +2,7 @@ export default function Hubspot() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>This is a Hubspot component.</p>
+        <p>This is a Hubspot component, where contacts will be shown.</p>
       </header>
     </div>
   );
