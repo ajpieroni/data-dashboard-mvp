b7bc740bf4c41980cb43a22053aac0e7d5c78353
Alex <akp42@duke.edu>
2024-07-15 06:55:45 -0700

    reconfigure lolo

--- src/App.js
+++ src/App.js
@@ -6,20 +6,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Out in Tech: Staff Data Dashboard
           <Hubspot />
           <Eventbrite />
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo" alt="logo" />
         <p className="text-sm text-gray-600 dark:text-gray-400 mt-3">
             © 2024 Out in Tech. All rights reserved.
           </p>
