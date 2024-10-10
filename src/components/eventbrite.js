f09fac32a22649da7e918d540b4b875a9ce67244
Alex <alexander.pieroni@duke.edu>
2024-10-10 07:19:57 -0400

    fix import

--- src/components/eventbrite.js
+++ src/components/eventbrite.js
@@ -1,8 +1,10 @@
+import UploadAndChart from "./UploadAndChart";
+
 export default function Eventbrite() {
     return (
       <div>
           <p>This is a Eventbrite component, where event data will be shown.</p>
-          
+
         <UploadAndChart />
 
       </div>
